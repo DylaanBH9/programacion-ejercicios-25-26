e18b8827c45dc6a8a121421123b86334e237a64d
dbautih <dbautih@iesrafaelaberti.es>
2025-10-01 13:22:12 +0200

    .gitignore modificado y ejercicio prueba de clase

---
+++ tema01/P1.2-Python_basico-RA1a/prueba.py
@@ -0,0 +1,10 @@
+def suma (a:int, b:int) -> int:
+	return (a + b)
+
+def main():
+	print("Mi primer programa")
+	c = suma(1, 2)
+	print("El valor de la suma es: "+ str(c))
+	
+if __name__ == "__main__":
+	main()
